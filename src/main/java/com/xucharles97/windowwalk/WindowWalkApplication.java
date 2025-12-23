e9c8a2b83f40c064483cd82dd97db005f2af9c69
Charles Xu <xucharles97@gmail.com>
2025-12-22 21:07:15 -0800

    Initial commit

---
+++ src/main/java/com/xucharles97/windowwalk/WindowWalkApplication.java
@@ -0,0 +1,13 @@
+package com.xucharles97.windowwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowWalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindowWalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xucharles97/windowwalk/WindowWalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xucharles97.windowwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowWalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
