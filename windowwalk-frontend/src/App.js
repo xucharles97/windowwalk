33ef79925bcb2c561a35e7815d01e8da0f9cb607
Charles Xu <xucharles97@gmail.com>
2026-02-01 00:28:05 -0500

    Adding LoginForm and basic endpoints

--- windowwalk-frontend/src/App.js
+++ windowwalk-frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import { Content } from "antd/lib/layout/layout";
+import { Layout } from "antd";
 
-function App() {
+const { Header, Content } = Layout;
+
+// start with: NODE_OPTIONS=--openssl-legacy-provider npm start
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ height: "100vh" }}>
+      <Header style={{ color: "white" }}>header</Header>
+      <Content
+        style={{
+          padding: "50px",
+          maxHeight: "calc(100% - 64px)",
+          overflowY: "auto",
+        }}
+      >
+        content
+      </Content>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ windowwalk-frontend/src/util.js
@@ -0,0 +1,82 @@
+export const login = (credentials) => {
+  const loginUrl = `/login?username=${credentials.username}&password=${credentials.password}`;
+  return fetch(loginUrl, {
+    method: "POST",
+    hdaders: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (Response.status < 200 || response.status >= 300) {
+      throw Error("failed to login");
+    }
+    return response.json();
+  });
+};
+
+export const signup = (data) => {
+  const signupUrl = "/signup";
+
+  return fetch(signupUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (Response.status < 200 || response.status >= 300) {
+      throw Error("failed to sign up");
+    }
+    return response.json();
+  });
+};
+
+export const getMenus = (restId) => {
+  return fetch(`/restaurant/${restId}/menu`).then((response) => {
+    if (Response.status < 200 || response.status >= 300) {
+      throw Error("failed to get menu");
+    }
+
+    return response.json();
+  });
+};
+
+export const getCart = () => {
+  return fetch("/cart").then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to get shopping cart data");
+    }
+
+    return response.json();
+  });
+};
+
+export const checkout = () => {
+  return fetch("/cart/checkout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to checkout");
+    }
+  });
+};
+
+export const addItemToCart = (itemId) => {
+  const payload = {
+    menu_id: itemId,
+  };
+
+  return fetch(`/cart`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      throw Error("Fail to add menu item to shopping cart");
+    }
+  });
+};
