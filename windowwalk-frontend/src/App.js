f2816a50b9843f18a008fa0c7bee969408c823f7
Charles Xu <xucharles97@gmail.com>
2026-02-16 00:05:11 -0500

    Adding Signup Form

--- windowwalk-frontend/src/App.js
+++ windowwalk-frontend/src/App.js
@@ -1,16 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 // import { Content } from "antd/lib/layout/layout";
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 const { Header, Content } = Layout;
+const { Title } = Typography;
 
 // start with: NODE_OPTIONS=--openssl-legacy-provider npm start
 
 const App = () => {
+  const [authed, setAuthed] = useState(false);
   return (
     <Layout style={{ height: "100vh" }}>
-      <Header style={{ color: "white" }}>header</Header>
+      <Header style={{ color: "white" }}>
+        <div className="header">
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            WindowWalk
+          </Title>
+        </div>
+      </Header>
       <Content
         style={{
           padding: "50px",
@@ -18,7 +31,11 @@ const App = () => {
           overflowY: "auto",
         }}
       >
-        content
+        {authed ? (
+          <div>content placeholder</div>
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
       </Content>
     </Layout>
   );
